e4194d97f9748f6e6646e471d67bd0bcbefa6994
FREDRICKKYEKI <101971767+FREDRICKKYEKI@users.noreply.github.com>
2023-05-24 19:49:52 +0300

    pop func

--- monty.h
+++ monty.h
@@ -67,6 +67,8 @@ extern globl_t globl_vars;
 void push(stack_t **head, unsigned int line_num);
 void pall(stack_t **head, unsigned int line_num);
 void pint(stack_t **head, unsigned int line_num);
+void pop(stack_t **head, unsigned int line_num)
+
 
 /*get operation codes function - get_opcodes*/
 void (*get_opcode_func(char *op_code))(stack_t **stack, unsigned int line_num);
